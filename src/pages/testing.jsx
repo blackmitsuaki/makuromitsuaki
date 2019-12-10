008320cd92fb8ba9b820b1182c5123a14015b1ad
blackmitsuaki <black.mitsuaki@protonmail.com>
2019-12-10 23:17:43 +0700

    shit

---
+++ src/pages/testing.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Testing = () => {
+  const getData = () => {
+    return "wadaw";
+  };
+  return <h1>Mantap {getData()} Combo</h1>;
+};
+
+export default Testing;
